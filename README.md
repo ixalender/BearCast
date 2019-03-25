73bb2c52f1e51094fab7e2ca477dd7936a0766a6
ixalender <anekrasov@fastmail.com>
2019-03-25 19:33:55 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BearCast
+
+Plugin for IntelliJ platform for saving pieces of source code to Bear app.
---
+++ src/main/kotlin/com/bearcast/BearCast.kt
@@ -0,0 +1,9 @@
+package com.bearcast
+
+import com.intellij.openapi.components.BaseComponent
+
+class BearCast : BaseComponent {
+    override fun getComponentName(): String {
+        return "BearCast"
+    }
+}
