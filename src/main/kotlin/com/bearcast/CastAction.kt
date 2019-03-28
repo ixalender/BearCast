19f25d10c535d69aaec38ae9eae511385bdeb939
ixalender <anekrasov@fastmail.com>
2019-03-28 22:08:45 +0300

    add simple MD syntax to exportable snippet

--- src/main/kotlin/com/bearcast/CastAction.kt
+++ src/main/kotlin/com/bearcast/CastAction.kt
@@ -1,5 +1,6 @@
 package com.bearcast
 
+import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -8,14 +9,19 @@ import org.apache.http.client.utils.URIBuilder
 class CastAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val ed = e.getData(CommonDataKeys.EDITOR) ?: return
+        val file = e.getData(CommonDataKeys.PSI_FILE) ?: return
+        val project = e.getData(CommonDataKeys.PROJECT) ?: return
+
+        val language = file.language.displayName.toLowerCase()
+        val textTemplate = """```$language${System.lineSeparator()}%s```"""
 
         val query = URIBuilder(ConfigRepo.load().bear.createUrl).apply {
-            addParameter("title", e.getData(CommonDataKeys.VIRTUAL_FILE)?.name)
-            addParameter("text", ed.selectionModel.selectedText)
-            addParameter("tags", listOf("Tag1").joinToString(","))
+            addParameter("title", "${project.name} - ${file.name}")
+            addParameter("text", textTemplate.format(ed.selectionModel.selectedText))
+            addParameter("tags", listOf(language).joinToString(","))
             build()
         }
 
-        Runtime.getRuntime().exec("open ${query.toString().replace("+", "%20")}")
+        BrowserUtil.browse(query.toString().replace("+", "%20"))
     }
 }
