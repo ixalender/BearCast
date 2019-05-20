783a60810158108c1bd877f33a519f544c60f886
Alexander Nekrasov <devest.avo@gmail.com>
2019-05-20 09:22:08 +0300

    wip: add base settings form

---
+++ src/main/kotlin/com/bearcast/form/SettingsFormBase.java
@@ -0,0 +1,10 @@
+package com.bearcast.form;
+
+import javax.swing.*;
+
+public class SettingsFormBase extends JDialog {
+    public JPanel contentPane;
+    public JButton buttonOK;
+    public JButton buttonCancel;
+    public JCheckBox chkProjectNameTag;
+}
---
+++ src/test/kotlin/com/bearcast/SettingsFormTest.kt
@@ -0,0 +1,10 @@
+package com.bearcast
+
+import com.bearcast.form.SettingsForm
+import org.junit.Test
+
+class SettingsFormTest {
+    @Test fun openFormTest() {
+        SettingsForm.main()
+    }
+}
