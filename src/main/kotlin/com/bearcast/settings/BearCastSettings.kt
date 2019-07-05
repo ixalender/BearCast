a29fc9562a9aebd4e3b1a1cb1a73c5cfd0137a0b
Alexander Nekrasov <devest.avo@gmail.com>
2019-07-06 01:40:02 +0300

    make language tag enabled by default
    + additional keymap for plugin activating

--- src/main/kotlin/com/bearcast/settings/BearCastSettings.kt
+++ src/main/kotlin/com/bearcast/settings/BearCastSettings.kt
@@ -2,5 +2,5 @@ package com.bearcast.settings
 
 data class  BearCastSettings(
     var addProjectNameTag: Boolean = false,
-    var addLanguageTag: Boolean = false
+    var addLanguageTag: Boolean = true
 )
